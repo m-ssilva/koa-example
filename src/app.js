eede561cfae192ae5a13342a43d6676fa1d04683
m-ssilva <bleed-it-out@live.com>
2019-02-06 00:52:17 -0200

    refactor(app): adding mongoose connect and bodyparser

--- src/app.js
+++ src/app.js
@@ -4,9 +4,14 @@ const router = require('koa-router')
 const Router = new router()
 const path = require('path')
 const registerRoutes = require('./helpers/register-routes')
+const bodyParser = require('koa-bodyparser')
+const config = require('./config')
+const mongoose = require('mongoose')
 
 registerRoutes(Router, path.join(__dirname, './routes'))
 
+app.use(bodyParser())
 app.use(Router.routes())
+mongoose.connect(config.mongodb.connString, config.mongodb.configs)
 
-app.listen(3000, () => { console.log('API running on port 3000') })
+app.listen(config.api.port, () => { console.log(`API running on ${config.api.port}`) })
